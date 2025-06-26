f43b1908d50d237e17f207af4ecf3c82f181f064
developerleonardo <leo.11hnz@gmail.com>
2025-06-26 09:51:10 -0500

    feat: add TemplateCard component and templates data for dynamic rendering of template options

---
+++ src/components/TemplateCard.tsx
@@ -0,0 +1,21 @@
+type TemplateCardProps = {
+  title: string;
+  imageUrl: string;
+};
+
+export const TemplateCard = ({ title, imageUrl }: TemplateCardProps) => {
+  return (
+    <div className="flex flex-col items-center rounded-lg bg-white border hover:border-black cursor-pointer transition-colors duration-200">
+      <figure className="w-full h-48 rounded-lg rounded-b-none">
+        <img
+          src={imageUrl}
+          alt={title}
+          className="w-full h-full rounded-lg object-cover rounded-b-none"
+        />
+      </figure>
+      <h3 className="mt-2 text-base font-semibold">{title}</h3>
+    </div>
+  );
+};
+
+// https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg
---
+++ src/data/templates.ts
@@ -0,0 +1,22 @@
+export const templates = [
+  {
+    title: "Classical",
+    url: "https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg",
+  },
+  {
+    title: "Modern",
+    url: "https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg",
+  },
+  {
+    title: "Minimalist",
+    url: "https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg",
+  },
+  {
+    title: "Rustic",
+    url: "https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg",
+  },
+  {
+    title: "Industrial",
+    url: "https://cdn.pixabay.com/photo/2021/08/17/23/50/vintage-6554322_960_720.jpg",
+  },
+];
