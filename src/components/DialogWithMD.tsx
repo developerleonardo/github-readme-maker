f4607839085e6d249f4cb6426200838284eab24f
developerleonardo <leo.11hnz@gmail.com>
2025-07-01 14:55:53 -0500

    fix: update SVG attributes to camelCase in DialogWithMD component

--- src/components/DialogWithMD.tsx
+++ src/components/DialogWithMD.tsx
@@ -69,9 +69,9 @@ export const DialogWithMD = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="lucide lucide-github-icon lucide-github"
               >
                 <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
@@ -80,9 +80,9 @@ export const DialogWithMD = () => {
               Start on Github{" "}
               <svg
                 className="text-yellow-500"
-                stroke-linejoin="round"
-                stroke-linecap="round"
-                stroke-width="2"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2"
                 stroke="currentColor"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -114,9 +114,9 @@ export const DialogWithMD = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="lucide lucide-copy-icon lucide-copy"
                 >
                   <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
@@ -133,9 +133,9 @@ export const DialogWithMD = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="lucide lucide-check-icon lucide-check"
                 >
                   <polyline points="20 6 9 17 4 12" />
